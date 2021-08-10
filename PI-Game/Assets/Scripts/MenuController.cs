446d8e5bbcdc88ef75614274ea1786fbaa483cd9
Marcely Melo <56039260+MarcyMellow@users.noreply.github.com>
2021-08-10 13:54:52 -0300

    Menu Completinho

--- PI-Game/Assets/Scripts/MenuController.cs
+++ PI-Game/Assets/Scripts/MenuController.cs
@@ -1,12 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine;
 
 public class MenuController : MonoBehaviour
 {
-   
-    public void changeScene(){
-    
+    [SerializeField] int nomeCena;
+    public Image credits;
+    void Update(){
+        if (Input.GetKeyDown(KeyCode.Escape))
+         sair();
+    }
+    public void trocarCena(){
+        Time.timeScale=1f;
+        SceneManager.LoadScene(nomeCena);
+    }
+    public void creditos(){
+            credits.gameObject.SetActive(true);
+    }
+    void sair(){
+            Application.Quit();
     }
 }
